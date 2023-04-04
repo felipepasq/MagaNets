5e41d5329caba0320e803ca472deb7deaca34d30
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2023-04-04 19:31:15 -0300

    installed react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Router'
 function App() {
-  return <h1>Hello Maganets</h1>
+  return (
+    <>
+      <h1>Header</h1>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App
