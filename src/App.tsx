4458dc642b5333337b4fa32a21448b06391234b9
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2023-04-04 20:00:15 -0300

    configured global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import GlobalStyle from './styles/global'
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Header</h1>
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  colors: {
+    primary: '#5A2D82',
+    white: '#ffffff'
+  },
+  fonts: {
+    text: 'Roboto'
+  }
+}
